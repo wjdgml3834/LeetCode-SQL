ca43a860c62121ef37bae53e5077b9dfb6c2ec94
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-10-15 15:05:45 +0900

    Time: 530 ms (97.74%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,7 +1,5 @@
-SELECT ROUND(SUM(CASE 
-                    WHEN order_date = customer_pref_delivery_date THEN 1 
-                    ELSE 0 
-                 END) / COUNT(*) * 100, 2) AS immediate_percentage
+SELECT ROUND(SUM(CASE WHEN DATEDIFF(order_date,customer_pref_delivery_date) = 0 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) 
+AS immediate_percentage
 FROM (
     SELECT *
         , ROW_NUMBER () OVER (PARTITION BY customer_id ORDER BY order_date) AS rn
