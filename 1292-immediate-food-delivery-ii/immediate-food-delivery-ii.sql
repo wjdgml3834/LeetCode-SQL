df856ee2cc74c16e25411267e8a00834c972928c
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-10-15 15:31:01 +0900

    Time: 803 ms (19.42%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,8 +1,15 @@
-SELECT ROUND(SUM(CASE WHEN DATEDIFF(order_date,customer_pref_delivery_date) = 0 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) 
-AS immediate_percentage
-FROM (
-    SELECT *
-        , ROW_NUMBER () OVER (PARTITION BY customer_id ORDER BY order_date) AS rn
+WITH delivery_category AS ( -- immediate, scheduled 구분 테이블 
+    SELECT CASE WHEN order_date = customer_pref_delivery_date THEN 'immediate' ELSE 'scheduled' END AS delivery_status
+         , customer_id
+         , order_date
     FROM Delivery
-) AS sub
-WHERE rn = 1
+),
+first_order AS ( -- 고객 id별 첫 주문인지 판단 테이블 
+    SELECT customer_id 
+         , MIN(order_date) AS min_date
+    FROM Delivery
+    GROUP BY customer_id
+)
+SELECT ROUND(SUM(CASE WHEN delivery_status = 'immediate' THEN 1 ELSE 0 END) / COUNT(*) * 100 ,2) AS immediate_percentage
+FROM delivery_category dc
+    INNER JOIN first_order fo ON dc.customer_id = fo.customer_id AND dc.order_date = fo.min_date 
