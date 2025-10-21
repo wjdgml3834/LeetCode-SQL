f26796df3bba3c2abc454903473e7e486a4065fa
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-10-21 15:23:46 +0900

    Time: 483 ms (95.79%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,12 @@
+SELECT ROUND(SUM(logged_again) / COUNT(*),2) AS fraction
+FROM (
+    SELECT fl.player_id
+        , CASE WHEN a.event_date IS NOT NULL THEN 1 ELSE 0 END AS logged_again
+    FROM (
+        SELECT player_id
+            , MIN(event_date) AS first_login
+        FROM Activity
+        GROUP BY player_id
+    ) AS fl
+        LEFT JOIN Activity a ON fl.player_id = a.player_id AND DATE_ADD(fl.first_login, INTERVAL 1 DAY) = a.event_date
+) AS sub
