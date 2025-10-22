10373584fe756c5be4daf99661bbd0e95f8be98f
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-10-22 17:50:18 +0900

    Time: 1500 ms (21.06%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,8 +1,7 @@
-SELECT v.customer_id
+-- 1. GROUP BY + CASE WHEN -> COUNT 1 ELSE 0 -> SUM
+SELECT customer_id
      , SUM(CASE WHEN t.transaction_id IS NULL THEN 1 ELSE 0 END) AS count_no_trans
 FROM Visits v
     LEFT JOIN Transactions t ON v.visit_id = t.visit_id
-GROUP BY v.customer_id
+GROUP BY customer_id
 HAVING count_no_trans != 0 
-
-
