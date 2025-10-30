dc5710d89a13d2b49b326ad9b4e143a1af92a951
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-10-30 16:21:36 +0900

    Time: 912 ms (62.09%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,9 +1,9 @@
 SELECT st.student_id
-      , student_name
-      , sub.subject_name
-      , SUM(CASE WHEN e.student_id IS NULL THEN 0 ELSE 1 END) AS attended_exams
+     , st.student_name
+     , sb.subject_name
+     , SUM(CASE WHEN e.student_id IS NOT NULL THEN 1 ELSE 0 END) AS attended_exams
 FROM Students st
-    CROSS JOIN Subjects sub
-    LEFT JOIN Examinations e ON st.student_id = e.student_id AND e.subject_name = sub.subject_name
-GROUP BY st.student_id, st.student_name, sub.subject_name
-ORDER BY st.student_id, sub.subject_name
+    CROSS JOIN Subjects sb
+    LEFT JOIN Examinations e ON st.student_id = e.student_id AND sb.subject_name = e.subject_name
+GROUP BY st.student_id, st.student_name, sb.subject_name
+ORDER BY st.student_id, st.student_name
