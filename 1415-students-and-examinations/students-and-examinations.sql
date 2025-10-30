ab2aa7fec68865c1112043310349d0014d5981ee
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-10-30 17:04:59 +0900

    Time: 843 ms (87.51%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,8 +1,8 @@
--- 1. CROSS JOIN  + SUM (CASE WHEN)
+-- 2. HOW TO CALCULATE attended_exams -> COUNT(column)
 SELECT st.student_id
      , st.student_name
      , sb.subject_name
-     , SUM(CASE WHEN e.student_id IS NOT NULL THEN 1 ELSE 0 END) AS attended_exams
+     , COUNT(e.student_id) AS attended_exams
 FROM Students st
     CROSS JOIN Subjects sb 
     LEFT JOIN Examinations e ON st.student_id = e.student_id AND sb.subject_name = e.subject_name
