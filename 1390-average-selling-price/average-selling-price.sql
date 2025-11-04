ba007eb3371c4b3f4b8af4925a967b6bdde22ecd
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-04 17:25:57 +0900

    Time: 778 ms (43.50%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,10 +1,11 @@
-SELECT P.product_id, 
-       CASE WHEN SUM(units) != 0 
-            THEN ROUND(SUM(price * units) / SUM(units), 2) 
-            ELSE 0 
-       END AS average_price
-FROM Prices P
-    LEFT JOIN UnitsSold US ON P.product_id = US.product_id
-      AND US.purchase_date >= P.start_date 
-      AND US.purchase_date <= end_date
-GROUP BY P.product_id;
+SELECT product_id
+     , CASE WHEN SUM(units) IS NOT NULL THEN ROUND(SUM(total_price) / SUM(units),2) ELSE 0 END AS average_price
+FROM (
+    SELECT p.product_id
+        , p.price
+        , u.units
+        , price * units AS total_price
+    FROM Prices p
+        LEFT JOIN UnitsSold u ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+) sub
+GROUP BY product_id
