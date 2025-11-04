977cc5ce6289782ce2df2731371991bbec9053f2
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-04 18:07:52 +0900

    Time: 699 ms (79.86%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,11 +1,16 @@
+-- 1. join
+-- 2. select only puchase date is in between start_date and end_date
+-- 3. SUB QUERY + GROUP BY -> calculate average_price
+
 SELECT product_id
-     , CASE WHEN SUM(units) != 0 THEN ROUND(SUM(total_price) / SUM(units),2) ELSE 0 END AS average_price
+     , CASE WHEN SUM(units) !=0 THEN ROUND(SUM(total_price) / SUM(units),2) ELSE 0 END AS average_price
 FROM (
     SELECT p.product_id
         , p.price
-        , u.units
-        , price * units AS total_price
+        , us.units
+        , p.price * us.units AS total_price
     FROM Prices p
-        LEFT JOIN UnitsSold u ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
-) sub
+        LEFT JOIN UnitsSold us ON p.product_id = us.product_id
+        AND us.purchase_date BETWEEN p.start_date AND p.end_date
+) AS sub
 GROUP BY product_id
