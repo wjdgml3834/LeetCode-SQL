dc6905541a1e11bf5e533cb0649ae383c539cc1b
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-06 17:45:43 +0900

    Time: 1281 ms (10.63%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,8 +1,14 @@
--- 1. select subquery 
+-- 2. window function to get total numbers of users
 
 SELECT r.contest_id
-     , ROUND(COUNT(DISTINCT r.user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users) * 100 ,2) AS percentage 
-FROM Users u
+     , ROUND(COUNT(DISTINCT r.user_id) /  u.total_user * 100 ,2) AS percentage
+FROM (
+    SELECT user_id
+         , user_name
+         , COUNT(*) OVER () AS total_user
+    FROM Users
+) AS u 
     INNER JOIN Register r ON u.user_id = r.user_id
 GROUP BY r.contest_id
-ORDER BY percentage DESC, r.contest_id ASC
+ORDER BY percentage DESC, r.contest_id ASC;
+
