e7171b4e776ec7190fbffbebb7c4ac6b5af08525
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-06 17:39:41 +0900

    Time: 983 ms (38.85%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,6 +1,8 @@
+-- 1. select subquery 
+
 SELECT r.contest_id
-     , ROUND(COUNT(DISTINCT r.user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users) * 100,2) AS percentage
+     , ROUND(COUNT(DISTINCT r.user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users) * 100 ,2) AS percentage 
 FROM Users u
     INNER JOIN Register r ON u.user_id = r.user_id
 GROUP BY r.contest_id
-ORDER BY percentage DESC, r.contest_id ASC;
+ORDER BY percentage DESC, r.contest_id ASC
