ab4128768dfebf09d73dcbd48645628aa92d8145
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-06 16:30:29 +0900

    Time: 854 ms (82.47%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,5 @@
 SELECT r.contest_id
-     , ROUND(COUNT(DISTINCT r.user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users) * 100,2) AS percentage
+     , ROUND(COUNT(DISTINCT r.user_id) / (SELECT COUNT(user_id) FROM Users) * 100,2) AS percentage
 FROM Users u
     LEFT JOIN Register r ON u.user_id = r.user_id
 WHERE r.contest_id IS NOT NULL 
