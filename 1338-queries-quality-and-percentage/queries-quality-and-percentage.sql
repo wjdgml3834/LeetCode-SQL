c9289176006c161af6315f987b4da31f28c7a4e1
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-11 16:54:51 +0900

    Time: 340 ms (59.68%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,7 +1,7 @@
+-- thinking:  divide and conquer 
 
 SELECT query_name
-     , ROUND(SUM(rating / position) / COUNT(*),2) AS quality
-     , ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100/ COUNT(*),2) AS poor_query_percentage 
+     , ROUND(SUM( rating / position ) / COUNT(*),2) AS quality
+     , ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100,2) AS poor_query_percentage
 FROM Queries
 GROUP BY query_name
-
