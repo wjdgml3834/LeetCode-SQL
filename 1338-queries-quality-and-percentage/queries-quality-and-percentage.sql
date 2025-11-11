ca096dc439b78d816241c34fc1f1735bd5f13c80
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-11 16:16:07 +0900

    Time: 371 ms (27.46%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,19 +1,38 @@
--- 1. quality
-WITH query_quality AS (
+WITH quality_ratio AS (
 SELECT query_name
-     , ROUND(SUM(rating / position) / COUNT(*),2) AS quality
-FROM Queries
+     , ROUND(SUM(quality_ratio) / COUNT(*),2) AS quality
+FROM (
+    SELECT query_name
+        , position
+        , rating
+        , rating / position AS quality_ratio
+    FROM Queries
+) AS sub 
 GROUP BY query_name
 ),
--- 2. poor_query_percentage
 poor_query AS (
-SELECT query_name
-     , ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100/ COUNT(*),2) AS poor_query_percentage 
-FROM Queries
-GROUP BY query_name
-) 
-SELECT qq.query_name
-     , qq.quality
-     , pq.poor_query_percentage
-FROM query_quality qq
-    INNER JOIN poor_query pq ON qq.query_name = pq.query_name
+SELECT total_cnt.query_name
+     , CASE 
+            WHEN ROUND((poor / total) * 100,2) IS NULL THEN 0 
+            ELSE ROUND((poor / total) * 100,2)
+       END AS poor_query_percentage
+FROM (
+    SELECT query_name
+        , COUNT(*) AS total
+    FROM Queries
+    GROUP BY query_name
+) AS total_cnt
+LEFT JOIN (
+    SELECT query_name
+        , COUNT(*) AS poor
+    FROM Queries
+    WHERE rating < 3 
+    GROUP BY query_name
+) AS poor_cnt
+ON total_cnt.query_name = poor_cnt.query_name
+)
+SELECT qr.query_name
+     , quality
+     , poor_query_percentage
+FROM quality_ratio qr
+    INNER JOIN poor_query pq ON qr.query_name = pq.query_name
