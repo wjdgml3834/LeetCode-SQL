9ab5dd1e2b49f5c3220347c2605e3a5fdef5f986
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-11 16:14:44 +0900

    Time: 336 ms (64.71%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,5 +1,19 @@
+-- 1. quality
+WITH query_quality AS (
 SELECT query_name
      , ROUND(SUM(rating / position) / COUNT(*),2) AS quality
-     , ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100,2) AS poor_query_percentage
 FROM Queries
 GROUP BY query_name
+),
+-- 2. poor_query_percentage
+poor_query AS (
+SELECT query_name
+     , ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100/ COUNT(*),2) AS poor_query_percentage 
+FROM Queries
+GROUP BY query_name
+) 
+SELECT qq.query_name
+     , qq.quality
+     , pq.poor_query_percentage
+FROM query_quality qq
+    INNER JOIN poor_query pq ON qq.query_name = pq.query_name
