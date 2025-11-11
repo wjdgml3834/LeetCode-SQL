273bb39bd6629e9638e8c1dc63d7217d1241f24d
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-11 16:19:20 +0900

    Time: 396 ms (15.79%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,38 +1,7 @@
-WITH quality_ratio AS (
+
 SELECT query_name
-     , ROUND(SUM(quality_ratio) / COUNT(*),2) AS quality
-FROM (
-    SELECT query_name
-        , position
-        , rating
-        , rating / position AS quality_ratio
-    FROM Queries
-) AS sub 
+     , ROUND(SUM(rating / position) / COUNT(*),2) AS quality
+     , ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100/ COUNT(*),2) AS poor_query_percentage 
+FROM Queries
 GROUP BY query_name
-),
-poor_query AS (
-SELECT total_cnt.query_name
-     , CASE 
-            WHEN ROUND((poor / total) * 100,2) IS NULL THEN 0 
-            ELSE ROUND((poor / total) * 100,2)
-       END AS poor_query_percentage
-FROM (
-    SELECT query_name
-        , COUNT(*) AS total
-    FROM Queries
-    GROUP BY query_name
-) AS total_cnt
-LEFT JOIN (
-    SELECT query_name
-        , COUNT(*) AS poor
-    FROM Queries
-    WHERE rating < 3 
-    GROUP BY query_name
-) AS poor_cnt
-ON total_cnt.query_name = poor_cnt.query_name
-)
-SELECT qr.query_name
-     , quality
-     , poor_query_percentage
-FROM quality_ratio qr
-    INNER JOIN poor_query pq ON qr.query_name = pq.query_name
+
