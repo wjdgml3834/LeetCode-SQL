2886bd2ebb50f3752fbf72e40992a7176f203d93
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-13 16:32:29 +0900

    Time: 650 ms (8.07%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,6 +1,7 @@
-# 일단 뭘 해야할까?
-SELECT activity_date As day
+-- 2. GROUP BY activity_date + COUNT(DISTINCT user_id)
+
+SELECT activity_date AS day
      , COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'   
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
 GROUP BY activity_date
