ccabd8b409c505c89f959b12897b3a5884a59431
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-18 16:58:11 +0900

    Time: 400 ms (56.95%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,8 @@
 -- 1. group by + count + HAVING
 -- 2. num is exist -> MAX(num) ELSE return NULL 
+-- 3. MORE EASIEST WAY 
 
-SELECT CASE WHEN num IS NOT NULL THEN MAX(num) ELSE NULL END AS num
+SELECT MAX(num) AS num 
 FROM (
     SELECT num
         , COUNT(*) AS cnt
