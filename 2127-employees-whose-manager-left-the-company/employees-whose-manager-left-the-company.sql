d6bcf73cd8acd8ecf046f36b86adec8e4ae0b66e
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-19 17:54:50 +0900

    Time: 332 ms (45.37%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,9 +1,7 @@
--- 1. less $30000
-SELECT e1.employee_id
-FROM Employees e1
-    LEFT JOIN Employees e2 ON e1.manager_id = e2.employee_id
-WHERE e2.employee_id IS NULL 
-      AND e1.salary < 30000
-      AND e1.manager_id IS NOT NULL
-ORDER BY e1.employee_id
+SELECT employee_id
+FROM Employees 
+WHERE manager_id IS NOT NULL
+      AND manager_id NOT IN (SELECT employee_id FROM Employees)
+      AND salary < 30000
+ORDER BY employee_id
 
