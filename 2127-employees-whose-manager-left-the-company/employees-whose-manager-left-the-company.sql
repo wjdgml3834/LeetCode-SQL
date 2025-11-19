3e83ea656e0415cf6bb9fbb64fd800108e42fec8
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-19 18:05:55 +0900

    Time: 281 ms (99.43%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,6 +1,6 @@
 SELECT employee_id
 FROM Employees 
-WHERE manager_id NOT IN (SELECT employee_id FROM Employees)
+WHERE manager_id IS NOT NULL
+      AND manager_id NOT IN (SELECT employee_id FROM Employees)
       AND salary < 30000
-ORDER BY employee_id
-
+ORDER BY 1 -- 1하면 첫번째 컬럼 기준으로 정렬하라는 뜻이다. 
