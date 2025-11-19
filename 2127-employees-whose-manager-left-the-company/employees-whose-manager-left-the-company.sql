7bf717135e20e88c607411ffbce135a975d049b6
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-11-19 17:56:45 +0900

    Time: 310 ms (75.13%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,7 +1,6 @@
 SELECT employee_id
 FROM Employees 
-WHERE manager_id IS NOT NULL
-      AND manager_id NOT IN (SELECT employee_id FROM Employees)
+WHERE manager_id NOT IN (SELECT employee_id FROM Employees)
       AND salary < 30000
 ORDER BY employee_id
 
