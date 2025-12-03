11fb213da66a4c06aa5d56f34e30c3a138535ff8
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-12-03 16:08:19 +0900

    Time: 772 ms (6.40%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,9 +1,20 @@
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
-   OR employee_id IN (
+(
+    SELECT employee_id
+        , department_id
+    FROM Employee
+    WHERE employee_id IN(
         SELECT employee_id
         FROM Employee
         GROUP BY employee_id
-        HAVING COUNT(*) = 1
-    );
+        HAVING COUNT(*) = 1 
+    )
+)
+UNION ALL
+
+(
+SELECT employee_id
+     , department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+)
+
