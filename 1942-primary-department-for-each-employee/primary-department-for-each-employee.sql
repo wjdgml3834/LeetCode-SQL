ffe42e503e3725ff95d3fdeab31fd0e2af92ac59
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-12-03 16:52:38 +0900

    Time: 586 ms (18.31%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,9 +1,19 @@
-SELECT employee_id, department_id
+-- FIRST WAY: divide -> UNION ALL 
+
+-- 1. employee who has only unuique department
+(
+SELECT employee_id
+      , MAX(department_id) AS department_id
 FROM Employee
-WHERE primary_flag = 'Y' 
-OR employee_id IN (
-    SELECT employee_id
-    FROM Employee
-    GROUP BY employee_id
-    HAVING COUNT(*) = 1
+GROUP BY employee_id
+HAVING COUNT(*) = 1 
+)
+UNION ALL
+
+(
+-- 2. multiple department
+SELECT employee_id
+     , department_id
+FROM Employee
+WHERE primary_flag = 'Y'
 )
