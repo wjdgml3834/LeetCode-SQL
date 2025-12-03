52bd591ddfbc2b07d94233b329557052c44b5e60
Junghee Kim <88475978+wjdgml3834@users.noreply.github.com>
2025-12-03 16:17:13 +0900

    Time: 487 ms (81.82%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,10 +1,9 @@
-SELECT employee_id
-     , department_id
-FROM (
-SELECT employee_id, 
-       department_id, 
-       primary_flag,
-       COUNT(*) OVER (PARTITION BY employee_id) AS dept_cnt
+SELECT employee_id, department_id
 FROM Employee
-) AS sub
-WHERE dept_cnt = 1 OR (dept_cnt >=2 AND primary_flag = 'Y')
+WHERE primary_flag = 'Y' 
+OR employee_id IN (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(*) = 1
+)
